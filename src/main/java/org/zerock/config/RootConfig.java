0eebc058256875ba560bf0d9f6a9b6bc16235e2d
ghd075 <rlfls54@gmail.com>
2019-12-14 16:12:36 +0900

    Initial commit

---
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,26 @@
+package org.zerock.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"org.zerock"})
+public class RootConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/spring?characterEncoding=utf8&useSSL=false");
+		hikariConfig.setUsername("gugu");
+		hikariConfig.setPassword("1234");
+		
+		return new HikariDataSource(hikariConfig);
+	}
+}
